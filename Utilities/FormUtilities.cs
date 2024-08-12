6adeb1143412fbb50fa8057ac8755f0942edfae5
SteveGongoraL <stv.brayton.gongora@gmail.com>
2024-08-12 01:02:15 -0600

    Agregando clase para validar el llenado de los textbox y combobox

---
+++ Utilities/FormUtilities.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GobData.Utilities
+{
+    public static class FormUtilities
+    {
+        public static bool ValidarTextBox(Control parent)
+        {
+            bool allFilled = true;
+
+            foreach (Control control in parent.Controls)
+            {
+                if (control is TextBox txtBox)
+                {
+                    if (String.IsNullOrWhiteSpace(txtBox.Text))
+                    {
+                        allFilled = false;
+                        break;
+                    }
+                }
+            }
+
+            return allFilled;
+        }
+
+        public static bool ValidarComboBox(Control parent)
+        {
+            bool allSelected = true;
+
+            foreach (Control control in parent.Controls)
+            {
+                if (control is ComboBox comboBox)
+                {
+                    if (comboBox.SelectedIndex == -1)
+                    {
+                        allSelected = false;
+                        break;
+                    }
+                }
+            }
+
+            return allSelected;
+        }
+
+        public static void LimpiarTextBox(Control parent)
+        {
+            foreach (Control control in parent.Controls)
+            {
+                if (control is TextBox txtBox)
+                {
+                    txtBox.Clear();
+                }
+            }
+        }
+    }
+}
