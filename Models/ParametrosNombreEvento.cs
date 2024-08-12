83e0ca0c35e3c8cdf41879c829c8988b45d4853d
SteveGongoraL <stv.brayton.gongora@gmail.com>
2024-08-12 16:21:43 -0600

    Agregando la carpeta para modelos junto con el primer modelo de los parametros nombre del evento

---
+++ Models/ParametrosNombreEvento.cs
@@ -0,0 +1,12 @@
+﻿namespace GobData
+{
+    public class ParametrosNombreEvento
+    {
+        public string Mes { get; set; }
+        public int Dia { get; set; }
+        public string Estado { get; set; }
+        public string Convocante { get; set; }
+        public string NumeroEvento { get; set; }
+        public string Division { get; set; }
+    }
+}
