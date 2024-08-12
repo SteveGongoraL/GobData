d6ac2188d8d995de8854cae82ac2dea33866d496
SteveGongoraL <stv.brayton.gongora@gmail.com>
2024-08-12 16:22:48 -0600

    Agregando la conexion a la db para guardar los datos del nombre del evento

---
+++ DataAccess/NombreEvento.cs
@@ -0,0 +1,35 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace GobData
+{
+    public class NombreEvento
+    {
+        private string connectionString;
+
+        public NombreEvento(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        // Los parametros vienen del modelo "ParametrosNombreEvento"
+        public void InsertEventName(ParametrosNombreEvento parametrosNombreEvento)
+        {
+            using (MySqlConnection conexionBD = new MySqlConnection(connectionString))
+            {
+                string consulta = "INSERT INTO NombreEvento(Mes, Dia, Estado, Convocante, Numero_Evento, Division) VALUES (@Mes, @Dia, @Estado, @Convocante, @Numero_Evento, @Division);";
+                using (MySqlCommand comando = new MySqlCommand(consulta, conexionBD))
+                {
+                    comando.Parameters.AddWithValue("@Mes", parametrosNombreEvento.Mes);
+                    comando.Parameters.AddWithValue("@Dia", parametrosNombreEvento.Dia);
+                    comando.Parameters.AddWithValue("@Estado", parametrosNombreEvento.Estado);
+                    comando.Parameters.AddWithValue("@Convocante", parametrosNombreEvento.Convocante);
+                    comando.Parameters.AddWithValue("@Numero_Evento", parametrosNombreEvento.NumeroEvento);
+                    comando.Parameters.AddWithValue("@Division", parametrosNombreEvento.Division);
+
+                    conexionBD.Open();
+                    comando.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
