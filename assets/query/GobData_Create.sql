a017b44a064b6079d35ea33dbe38f44ac4245906
SteveGongoraL <stv.brayton.gongora@gmail.com>
2024-08-13 00:35:38 -0600

    Se agrego la estructura para la tabla de partidas

--- assets/query/GobData_Create.sql
+++ assets/query/GobData_Create.sql
@@ -16,26 +16,24 @@ CREATE TABLE DocumentosEvento(
     NombreDocID INT NOT NULL,
     CONSTRAINT FK_NombreDoc FOREIGN KEY (NombreDocID) REFERENCES NombreEvento(IdCliente)
 )
-GO
 
-CREATE TABLE PartidasE(
+CREATE TABLE PartidasEvento(
     IdPartida INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     N_Partida INT NOT NULL,
     Descripcion VARCHAR(300) NOT NULL,
     Cant_Min INT NOT NULL,
     Cant_Max INT NOT NULL,
-    Unidad_Medida VARCHAR(50) NOT NULL,
+    Unidad_Medida VARCHAR(10) NOT NULL,
     Se_Ofrece VARCHAR(300) NOT NULL,
     Marca VARCHAR(50) NOT NULL,
     Catalogo VARCHAR(50) NOT NULL,
-    Pais_Origen VARCHAR(50) NOT NULL,
-    Fabricante VARCHAR(50) NOT NULL,
+    Pais_Origen VARCHAR(15) NOT NULL,
+    Fabricante VARCHAR(30) NOT NULL,
     Costo_Unitario DECIMAL(10,3) NOT NULL,
-    Moneda VARCHAR(10) NOT NULL,
+    Moneda VARCHAR(4) NOT NULL,
     Costo_Nacional DECIMAL(10,3) NOT NULL,
     Porcentaje_Ganancia DECIMAL(5,2) NOT NULL,
     Total_Unitario DECIMAL(10,3) NOT NULL,
     NombrePartID INT NOT NULL,
     CONSTRAINT FK_NombrePart FOREIGN KEY (NombrePartID) REFERENCES NombreEvento(IdCliente)
 )
-GO
