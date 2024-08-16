7a8ed92f81e1411d6294202c405abcee4d9c7207
SteveGongoraL <stv.brayton.gongora@gmail.com>
2024-08-16 01:56:14 -0600

    Se agrego un archivo de clases para las restricciones de los textbox

---
+++ Utilities/TextBoxValidator.cs
@@ -0,0 +1,36 @@
+﻿namespace GobData
+{
+    public static class TextBoxValidator
+    {
+        // Permitir solo texto
+        public static void AllowOnlyString(KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsLetter(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        // Permitir solo números enteros
+        public static void AllowOnlyIntegers(KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        // Permitir solo números y el punto decimal
+        public static void AllowOnlyNumbersAndDecimal(KeyPressEventArgs e, TextBox textBox)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != '.' && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+            else if (e.KeyChar == '.' && textBox.Text.Contains("."))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
