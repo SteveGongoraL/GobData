59911e543ef9d967ecc175fecb43b5f8290aa3b5
SteveGongoraL <stv.brayton.gongora@gmail.com>
2024-08-16 01:56:55 -0600

    Se agrego un nuevo modelo para los datos de las partidas

---
+++ Models/ParametrosPartida.cs
@@ -0,0 +1,22 @@
+﻿namespace GobData
+{
+    public class ParametrosPartida
+    {
+        public int NumeroPartida { get; set; }
+        public string Descripcion { get; set; }
+        public int CantidadMinima { get; set; }
+        public int CantidadMaxima { get; set; }
+        public string UnidadMedida { get; set; }
+        public string SeOfrece { get; set; }
+        public string Marca { get; set; }
+        public string Catalogo { get; set; }
+        public string PaisOrigen { get; set; }
+        public string Fabricante { get; set; }
+        public decimal CostoUnitario { get; set; }
+        public string Moneda { get; set; }
+        public decimal CostoNacional { get; set; }
+        public decimal PorcentajeGanancia { get; set; }
+        public decimal TotalUnitario { get; set; }
+        public int IdNombreEvento { get; set; }
+    }
+}
