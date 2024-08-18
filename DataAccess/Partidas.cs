55ba7074d09be1080d440a3a6427577460532011
SteveGongoraL <stv.brayton.gongora@gmail.com>
2024-08-18 17:41:16 -0600

    Se agrego un nuevo metodo para hacer update a la tabla de partidas

--- DataAccess/Partidas.cs
+++ DataAccess/Partidas.cs
@@ -82,6 +82,41 @@ namespace GobData
             return dataTable;
         }
 
+        // Metodo para actualizar los datos de la partida
+        public void UpdateDeparture(ParametrosPartida parametrosPartida)
+        {
+            using (MySqlConnection conexionBD = new MySqlConnection(connectionString))
+            {
+                string consulta = @"UPDATE PartidasEvento 
+                                    SET N_Partida = @N_Partida, Descripcion = @Descripcion, Cant_Min = @Cant_Min, Cant_Max = @Cant_Max, Unidad_Medida = @Unidad_Medida, Se_Ofrece = @Se_Ofrece,  Marca = @Marca, Catalogo = @Catalogo, Pais_Origen = @Pais_Origen, Fabricante = @Fabricante, Costo_Unitario = @Costo_Unitario, Moneda = @Moneda, Costo_Nacional = @Costo_Nacional, Porcentaje_Ganancia = @Porcentaje_Ganancia, Total_Unitario = @Total_Unitario
+                                    WHERE IdPartida = @IdPartida;";
+
+                using (MySqlCommand comando = new MySqlCommand(consulta, conexionBD))
+                {
+                    comando.Parameters.AddWithValue("@IdPartida", parametrosPartida.IdPartida);
+                    comando.Parameters.AddWithValue("@N_Partida", parametrosPartida.NumeroPartida);
+                    comando.Parameters.AddWithValue("@Descripcion", parametrosPartida.Descripcion);
+                    comando.Parameters.AddWithValue("@Cant_Min", parametrosPartida.CantidadMinima);
+                    comando.Parameters.AddWithValue("@Cant_Max", parametrosPartida.CantidadMaxima);
+                    comando.Parameters.AddWithValue("@Unidad_Medida", parametrosPartida.UnidadMedida);
+                    comando.Parameters.AddWithValue("@Se_Ofrece", parametrosPartida.SeOfrece);
+                    comando.Parameters.AddWithValue("@Marca", parametrosPartida.Marca);
+                    comando.Parameters.AddWithValue("@Catalogo", parametrosPartida.Catalogo);
+                    comando.Parameters.AddWithValue("@Pais_Origen", parametrosPartida.PaisOrigen);
+                    comando.Parameters.AddWithValue("@Fabricante", parametrosPartida.Fabricante);
+                    comando.Parameters.AddWithValue("@Costo_Unitario", parametrosPartida.CostoUnitario);
+                    comando.Parameters.AddWithValue("@Moneda", parametrosPartida.Moneda);
+                    comando.Parameters.AddWithValue("@Costo_Nacional", parametrosPartida.CostoNacional);
+                    comando.Parameters.AddWithValue("@Porcentaje_Ganancia", parametrosPartida.PorcentajeGanancia);
+                    comando.Parameters.AddWithValue("@Total_Unitario", parametrosPartida.TotalUnitario);
+                    comando.Parameters.AddWithValue("@NombrePartID", parametrosPartida.IdNombreEvento);
+
+                    conexionBD.Open();
+                    comando.ExecuteNonQuery();
+                }
+            }
+            return;
+        }
 
     }
 }
