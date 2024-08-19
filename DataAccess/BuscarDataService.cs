bd0a56e226f484f0d8519a255314bbe7cd4548cb
SteveGongoraL <stv.brayton.gongora@gmail.com>
2024-08-18 19:25:39 -0600

    Se agrego un nuevo metodo para hacer una busqueda en las partidas

--- DataAccess/BuscarDataService.cs
+++ DataAccess/BuscarDataService.cs
@@ -59,5 +59,41 @@ namespace GobData
             }
             return dataTable;
         }
+
+        // Metodo para buscar una partida en especifico
+        public DataTable SearchSpecificDeparture(string textoBusqueda, string idNombreEvento)
+        {
+            DataTable dataTable = new DataTable();
+            using (MySqlConnection conexionBD = new MySqlConnection(connectionString))
+            {
+                string queryConsulta = @"SELECT * FROM PartidasEvento WHERE NombrePartID = @idNombreEvento AND 
+                                        CONCAT(N_Partida, Descripcion, Cant_Min, Cant_Max, Unidad_Medida, Se_Ofrece, Marca, Catalogo, Pais_Origen, Fabricante, Costo_Unitario, Moneda, Costo_Nacional, Porcentaje_Ganancia, Total_Unitario, NombrePartID) LIKE @textoBusqueda;";
+                try
+                {
+                    using (MySqlCommand comando = new MySqlCommand(queryConsulta, conexionBD))
+                    {
+                        comando.Parameters.AddWithValue("@idNombreEvento", idNombreEvento);
+                        comando.Parameters.AddWithValue("@textoBusqueda", "%" + textoBusqueda + "%");
+                        conexionBD.Open();
+                        using (MySqlDataReader reader = comando.ExecuteReader())
+                        {
+                            dataTable.Load(reader);
+                        }
+                    }
+                }
+                catch (MySqlException ex)
+                {
+                    // Manejo de excepciones específicas de MySQL
+                    throw new Exception("Error al cargar datos desde MySQL: " + ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    // Manejo de otras excepciones
+                    throw new Exception("Error al cargar datos: " + ex.Message);
+                }
+            }
+            return dataTable;
+        }
+
     }
 }
