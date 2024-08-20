33cd9ad2ce02551b8b36ccc1e1d4ce9fe86dd0dc
SteveGongoraL <stv.brayton.gongora@gmail.com>
2024-08-20 00:14:34 -0600

    Se agrego una nueva clase para las propiedades de los documentos

---
+++ Models/ParametrosDocumentos.cs
@@ -0,0 +1,12 @@
+﻿namespace GobData
+{
+    // Objeto para almacenar las propiedades de un Documento
+    public class ParametrosDocumentos
+    {
+        // Propiedades del objeto
+        public string Nombre{ get; set; }
+        public string Direccion { get; set; }
+        public byte[] Documento { get; set; }
+        public int NombreDocID { get; set; }
+    }
+}
